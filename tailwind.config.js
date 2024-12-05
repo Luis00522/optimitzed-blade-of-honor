df2efbb2e6c4ce87cfa906b061d44d9ab4cdaad6
Luis <luis.ruizi@cirvianum.cat>
2024-12-05 17:55:25 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  content: ["./public/**/*.html", "./index.html", "./src/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'black-japan': '#2D2D2D',
+        'white-japan': '#F3F2E0' ,
+        'red-japan': '#8A0303'
+      },
+      fontFamily: {
+        serif: ['EB Garamond', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import path from "path";
+import imagemin from "vite-plugin-imagemin";
+import VitePluginWebpCompress from 'vite-plugin-webp-compress';
+
+export default defineConfig({
+  root: "./", // Directori root de Vite
+  base: "./", // Rutes relatives per als assets
+  build: {
+    outDir: "dist", // Directori de sortida per al build
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"), // Pàgina principal
+        login: path.resolve(__dirname, "src/login.html"),
+        charity: path.resolve(__dirname, "src/charityworkpage.html"),
+        contactus: path.resolve(__dirname, "src/contactus.html"),
+        dojo: path.resolve(__dirname, "src/dojo.html"),
+        dojoquotes: path.resolve(__dirname, "src/dojoquotes.html"),
+        home: path.resolve(__dirname, "src/home.html"),
+        realkatanas: path.resolve(__dirname, "src/realkatanas.html"),
+        signup: path.resolve(__dirname, "src/signup.html"),
+        aboutus: path.resolve(__dirname, "src/aboutus.html"),
+      },
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true, // Per assegurar que els canvis es detecten en alguns entorns
+    },
+  },
+  plugins: [
+    imagemin({
+      plugins: [
+        "imagemin-gifsicle",
+        "imagemin-mozjpeg",
+        "imagemin-pngquant",
+        "imagemin-svgo",
+      ],
+    }),
+    VitePluginWebpCompress(),
+  ],
+});
